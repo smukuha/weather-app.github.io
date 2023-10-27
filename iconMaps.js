09c5adca46458c84681f8d319dda3bd756567eab
Mukuha Safania <mukuhasafania@gmail.com>
2023-10-27 18:09:25 +0300

    cloud-shower-heavy svg update

--- iconMaps.js
+++ iconMaps.js
@@ -6,7 +6,7 @@ addMapping([0,1], "sun")
 addMapping([2], "cloud-sun")
 addMapping([3], "cloud")
 addMapping([45,48], "smog")
-addMapping([51,53,55,56,57,61,63,65,66,67,80,81,82], "cloud-shower-heavy")
+addMapping([51,53,55,56,57,61,63,65,66,67,80,81,82], "cloud-showers-heavy")
 addMapping([71,73,75,77,85,86], "snowflake")
 addMapping([95,96,99], "cloud-bolt")
 
